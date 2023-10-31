e806dab643fbf08ea748f460e985114eb06ef899
NeiNeiWasher <1660760819@qq.com>
2023-10-31 17:27:44 +0800

    feat: add test

--- app/src/main/java/com/zwp/mobilefacenet/MainActivity.java
+++ app/src/main/java/com/zwp/mobilefacenet/MainActivity.java
@@ -3,8 +3,10 @@ package com.zwp.mobilefacenet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 
+import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
@@ -56,10 +58,8 @@ public class MainActivity extends AppCompatActivity {
         imageViewCrop2 = findViewById(R.id.image_view_crop2);
         //裁剪人脸button
         Button cropBtn = findViewById(R.id.crop_btn);
-//        //活体识别button
-//        Button deSpoofingBtn = findViewById(R.id.de_spoofing_btn);
-//        //对比识别button
-//        Button compareBtn = findViewById(R.id.compare_btn);
+        //测试button
+        Button testBtn = findViewById(R.id.test_Btn);
         resultTextView = findViewById(R.id.result_text_view);
         resultTextView2 = findViewById(R.id.result_text_view2);
 
@@ -84,6 +84,17 @@ public class MainActivity extends AppCompatActivity {
                 faceCompare();
             }
         });
+
+        testBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                long start = System.currentTimeMillis();
+                faceCropTest();
+                faceCompare();
+                long end = System.currentTimeMillis();
+                resultTextView2.setText("总耗时:" + (end - start));
+            }
+        });
     }
 
     /**
@@ -97,6 +108,51 @@ public class MainActivity extends AppCompatActivity {
 
         Bitmap bitmapTemp1 = bitmap1.copy(bitmap1.getConfig(), false);
         Bitmap bitmapTemp2 = bitmap2.copy(bitmap1.getConfig(), false);
+
+        // 检测出人脸数据
+        long start = System.currentTimeMillis();
+        Vector<Box> boxes1 = mtcnn.detectFaces(bitmapTemp1, bitmapTemp1.getWidth() / 5); // 只有这句代码检测人脸，下面都是根据Box在图片中裁减出人脸
+        long end = System.currentTimeMillis();
+        resultTextView.setText("人脸检测前向传播耗时：" + (end - start));
+        resultTextView2.setText("");
+        Vector<Box> boxes2 = mtcnn.detectFaces(bitmapTemp2, bitmapTemp2.getWidth() / 5); // 只有这句代码检测人脸，下面都是根据Box在图片中裁减出人脸
+        if (boxes1.size() == 0 || boxes2.size() == 0) {
+            Toast.makeText(MainActivity.this, "未检测到人脸", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // 这里因为使用的每张照片里只有一张人脸，所以取第一个值，用来剪裁人脸
+        Box box1 = boxes1.get(0);
+        Box box2 = boxes2.get(0);
+
+        // 人脸矫正
+        bitmapTemp1 = Align.face_align(bitmapTemp1, box1.landmark);
+        bitmapTemp2 = Align.face_align(bitmapTemp2, box2.landmark);
+        boxes1 = mtcnn.detectFaces(bitmapTemp1, bitmapTemp1.getWidth() / 5);
+        boxes2 = mtcnn.detectFaces(bitmapTemp2, bitmapTemp2.getWidth() / 5);
+        box1 = boxes1.get(0);
+        box2 = boxes2.get(0);
+
+        box1.toSquareShape();
+        box2.toSquareShape();
+        box1.limitSquare(bitmapTemp1.getWidth(), bitmapTemp1.getHeight());
+        box2.limitSquare(bitmapTemp2.getWidth(), bitmapTemp2.getHeight());
+        Rect rect1 = box1.transform2Rect();
+        Rect rect2 = box2.transform2Rect();
+
+        // 剪裁人脸
+        bitmapCrop1 = MyUtil.crop(bitmapTemp1, rect1);
+        bitmapCrop2 = MyUtil.crop(bitmapTemp2, rect2);
+
+        imageViewCrop1.setImageBitmap(bitmapCrop1);
+        imageViewCrop2.setImageBitmap(bitmapCrop2);
+    }
+
+    private void faceCropTest() {
+
+        Resources res = getResources();
+        Bitmap bitmapTemp1 = BitmapFactory.decodeResource(res, R.drawable.asuka1);
+        Bitmap bitmapTemp2 = BitmapFactory.decodeResource(res, R.drawable.asuka2);
 
         // 检测出人脸数据
         long start = System.currentTimeMillis();
